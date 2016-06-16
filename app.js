1adadc133d8d01f2dc000d7307cb0c2367259a76
Timothy Shiu <timoweave@gmail.com>
2016-06-16 14:12:48 -0700

    assign 1

---
+++ app.js
@@ -0,0 +1,5 @@
+var message ="hello, hey there! what's up?";
+document.write(message);
+console.log(message);
+
+
---
+++ ionic/conFusion/www/js/services.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+        .constant("baseURL","http://localhost:3000/")
+        .constant("conFusionHistory", ["Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.", "The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan."])
+        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+            var promotions = [
+                {
+                          _id:0,
+                          name:'Weekend Grand Buffet', 
+                          image: 'images/buffet.png',
+                          label:'New',
+                          price:'19.99',
+                          description:'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person ',
+                }
+                
+            ];
+    
+                this.getDishes = function(){
+                    
+                    return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
+                    
+                };
+    
+                // implement a function named getPromotion
+                // that returns a selected promotion.
+                this.getPromotion = function() {
+                    return   $resource(baseURL+"promotions/:id");;
+                }
+    
+                        
+        }])
+
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+    
+            return $resource(baseURL+"leadership/:id");
+    
+        }])
+
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+    
+            return $resource(baseURL+"feedback/:id");
+    
+        }])
+;
