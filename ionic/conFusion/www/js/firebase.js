eb4dbeb18bd4dfee5c15f3e455f087ccd717638a
Timothy Shiu <timoweave@gmail.com>
2016-07-06 18:54:12 -0700

    add missing firebase config

---
+++ ionic/conFusion/www/js/firebase.js
@@ -0,0 +1,12 @@
+(function init_firebase() {
+
+  var config = {
+    apiKey: "AIzaSyAVQUhxbI774pL1p0wEzmD7Pk_FJWN7mRU",
+    authDomain: "confusionrest.firebaseapp.com",
+    databaseURL: "https://confusionrest.firebaseio.com",
+    storageBucket: "confusionrest.appspot.com",
+  };
+  
+  firebase.initializeApp(config);
+  
+})();
